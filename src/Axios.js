5edf43b8ae3a25cc4bbccc82240377b00d1e793d
krishnadev7 <krishnadevv12@gmail.com>
2021-08-26 13:21:54 +0530

    everything except order history successfully updated

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: '...'
-});
+    baseURL: 'http://localhost:5001/clone-86a0f/us-central1/api'
+})
 
 export default instance;
+
+//  website live link 👇
+
+// https://clone-86a0f.web.app/
